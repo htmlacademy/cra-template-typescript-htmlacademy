02861d7aeabf16cb0cb8bed88b7c3703f660c107
kam4atka <sledsib@ya.ru>
2022-06-20 17:54:45 +0300

    fix(package): Updated the React to version 18 (#11)
    * fix(package): Updated the React to version 18
    
    * fix(package): Updated the config of ESLint to 6.0.0
    
    * fix(package): Updated packages of testing-library
    
    * fix(template.json): Removed dublicate of the `engines` option
    
    * fix(template.json): Removed package `@testing-library/react-hooks`
    
    * feat(structure): Added folder `pages`

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'));
+);
